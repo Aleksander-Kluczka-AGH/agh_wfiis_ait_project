09bbdb1423e842c457fb0a142a8244ee0971fe0c
vis4rd <alagaesia.vrael@gmail.com>
2023-06-30 20:23:57 +0200

    Change default base URL to / in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/ait_project_2023/",
+    // base: "/ait_project_2023/", // FOR GITHUB PAGES DEPLOY
+    base: "/",
     plugins: [vue()],
     resolve: {
         alias: {
